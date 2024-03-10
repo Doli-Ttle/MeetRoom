9dc38a03f0b9bc32f56c510a2c94ba08fd073fcf
Yonnilight <yonnilight@gmail.com>
2024-03-10 18:57:01 +1300

    Meet Room V1

---
+++ slide.js
@@ -0,0 +1,9 @@
+
+        let sidenav = document.getElementById("sidenav");
+
+        function opennav() {
+            sidenav.style.right = "0";
+        }
+        function closenav() {
+            sidenav.style.right = "-200px";
+        }
